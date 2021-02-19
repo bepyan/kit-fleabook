83a9e5b3ebba45e5e04cb98d548047e95437af76
fhdufhdu <fhdufhdu@gmail.com>
2021-02-19 19:25:38 +0900

    ###21.02.19 공부용 테스트 페이지 추가

---
+++ src/components/UserMain.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="book_list">
+    <table>
+      <tbody>
+        <tr v-for="(row, index) in rows" v-bind:key="(row, index)">
+          <td>
+            <a @click="insertBook('./book_info/', row)">{{ row }}</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button id="insertBook" @click="insertBook('./book_info/', null)">
+      추가
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserMain",
+  data() {
+    let list = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      list.push(key);
+    }
+    return {
+      rows: list,
+    };
+  },
+  methods: {
+    insertBook: function (title) {
+      this.$router.push({ path: "/book_info", params: { title: title } });
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from "./components/HelloWorld";
+import UserMain from "./components/UserMain";
+import BookInfo from "./components/BookInfo";
 import Home from "./components/Home";
 import NonFound from "./components/NonFound";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-    { path: '/', component: HelloWorld },
-    { path: '/home/', component: Home },
+    { path: '/', component: UserMain },
+    { path: '/book_info/', component: BookInfo },
+    { path: '/reservation/', component: Home },
     { path: "*", component: NonFound }
   ]
 });
